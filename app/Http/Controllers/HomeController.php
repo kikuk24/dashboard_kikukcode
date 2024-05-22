a728684dca9429bf1606d69d3af8b6fd7463033f
kikuk24 <tirta.afandi@gmail.com>
2024-05-22 17:55:29 +0700

    Add Generate Sitemap

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Posts;
 use App\Models\Tutorial;
 use Illuminate\Http\Request;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 class HomeController extends Controller
 {
@@ -17,5 +19,27 @@ class HomeController extends Controller
             'tutorial' => $tutorials
         ];
         return view('welcome', $data);
+    }
+
+    public function sitemap()
+    {
+        $sitemap = Sitemap::create()
+            ->add(Url::create('/')->setPriority(1.0))
+            ->add(Url::create('/artikel')->setPriority(0.8));
+
+        $posts = Posts::all();
+
+        foreach ($posts as $post) {
+            $sitemap
+                ->add(Url::create('/artikel/' . $post->slug)->setPriority(0.6));
+        }
+
+        $tutorials = Tutorial::all();
+
+        foreach ($tutorials as $tutorial) {
+            $sitemap
+                ->add(Url::create('/tutorial/' . $tutorial->slug)->setPriority(0.6));
+        }
+        $sitemap->writeToFile(public_path('/sitemap.xml'));
     }
 }
